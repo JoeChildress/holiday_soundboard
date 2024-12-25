9655ab4f3f9032e368899095ec28a3d3c75d5faf
JoeChildress <joe.ch@comcast.net>
2024-12-25 06:59:02 -0800

    button state, prevent zoom

--- script.js
+++ script.js
@@ -33,20 +33,16 @@ const handleSoundClick = (btn, audioEl, loop) => {
     if (audioEl.loop === true) {
       audioEl.loop = false;
       audioEl.pause();
-      //btn.classList.remove('pressed');
     } else {
       audioEl.loop = true;
       audioEl.play();
-      // btn.classList.add('pressed');
     }
   } else {
-    //standard play
     audioEl.play();
-    //btn.classList.add('pressed');
     btn.classList.toggle('pressed');
     setTimeout(() => {
       btn.classList.toggle('pressed');
-    }, 2000);
+    }, 1500);
   }
 };
 
@@ -55,12 +51,13 @@ const buttons = document.querySelectorAll('.key-button');
 if (buttons?.length > 0) {
   [...buttons].map((btn) => {
     btn.addEventListener('click', (e) => {
-      console.log('clicked');
       const btn = e.target.closest('button');
       const keyVal = btn?.getAttribute('data-key') || 0;
       const isLoop = btn?.getAttribute('data-loop') || false;
       const audio = document.querySelector(`audio[data-Key="${keyVal}"]`);
-      console.log('keval and audio element', keyVal, isLoop, audio);
+      if (isLoop) {
+        btn.classList.toggle('pressed');
+      }
       handleSoundClick(btn, audio, isLoop);
     });
   });
